83e3c8c40f1fd1fce778ee9c26af9f05231d1383
Thompete <t.radzki16@gmail.com>
2023-05-18 05:47:53 +0200

    Initial commit

---
+++ src/test/java/com/thompete/conferenceservice/ConferenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thompete.conferenceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
